64898c3fa3640b82bee3b4e9d7be2d1882f07c9e
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-01 16:34:02 +0900

    add: リストをDoneにしてから2秒間操作がなかったら、そのリストを削除する機能

--- src/ts/Modules/DoneList/DoneList.ts
+++ src/ts/Modules/DoneList/DoneList.ts
@@ -2,36 +2,36 @@ import { todoUl } from "../../variables/variables.js";
 
 export const returnTargetList = (target: HTMLElement) => {
   let targetList;
-  if (target.parentElement?.parentElement?.classList.contains('todo__li')) {
+  if (target.parentElement?.parentElement?.classList.contains("todo__li")) {
     targetList = target.parentElement?.parentElement;
     return targetList;
   }
-  if (target.parentElement?.classList.contains('todo__li')) {
+  if (target.parentElement?.classList.contains("todo__li")) {
     targetList = target.parentElement;
     return targetList;
   }
 };
 const doneList = (target: HTMLElement) => {
   const targetList = returnTargetList(target);
-  const targetButton = targetList?.querySelector('.todo__button');
-  const targetText = targetList?.querySelector('.todo__text');
-  targetButton?.classList.toggle('todo__button--done');
-  targetText?.classList.toggle('todo__text--done');
+  const targetButton = targetList?.querySelector(".todo__button");
+  const targetText = targetList?.querySelector(".todo__text");
+  targetButton?.classList.toggle("todo__button--done");
+  targetText?.classList.toggle("todo__text--done");
 };
 export const DoneList = () => {
-  todoUl?.addEventListener('touchend', (e) => {
+  todoUl?.addEventListener("touchend", (e) => {
     e.preventDefault();
     const target = e.target as HTMLElement;
-    if (target.classList.contains('todo__button-area')) {
+    if (target.classList.contains("todo__button-area")) {
       doneList(target);
     }
-    if (target.classList.contains('todo__button')) {
+    if (target.classList.contains("todo__button")) {
       doneList(target);
     }
   });
-  todoUl?.addEventListener('mouseup', (e) => {
+  todoUl?.addEventListener("mouseup", (e) => {
     const target = e.target as HTMLButtonElement;
-    if (target.classList.contains('todo__button')) {
+    if (target.classList.contains("todo__button")) {
       doneList(target);
     }
   });
