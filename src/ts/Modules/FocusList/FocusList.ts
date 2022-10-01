923684dc8b0e7866bc87c573c5674cc92a759fa7
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-01 20:12:06 +0900

    fix: フォーカスが入るのをタッチが離れた時に修正した

--- src/ts/Modules/FocusList/FocusList.ts
+++ src/ts/Modules/FocusList/FocusList.ts
@@ -2,12 +2,12 @@ import { todoUl } from "../../variables/variables.js";
 import { returnTargetList } from "../DoneList/DoneList.js";
 
 export const FocusList = () => {
-  todoUl?.addEventListener('touchend', (e) => {
+  todoUl?.addEventListener("touchend", (e) => {
     const target = e.target as HTMLElement;
     const targetList = returnTargetList(target);
-    const targetNode = targetList?.querySelector('.todo__text')
+    const targetNode = targetList?.querySelector(".todo__text")
       ?.firstChild as Node;
-    const targetText = targetList?.querySelector('.todo__text') as HTMLElement;
+    const targetText = targetList?.querySelector(".todo__text") as HTMLElement;
     const targetTextLength = targetText.textContent?.length as number;
     const selection = window.getSelection();
     const range = document.createRange();
@@ -16,8 +16,8 @@ export const FocusList = () => {
     selection?.removeAllRanges();
     selection?.addRange(range);
     if (
-      target.classList.contains('todo__content') ||
-      target.classList.contains('todo__text')
+      target.classList.contains("todo__content") ||
+      target.classList.contains("todo__text")
     ) {
       targetText.focus();
     }
