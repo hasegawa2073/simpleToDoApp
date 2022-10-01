47ba4b166c314baf7b8efe6fc8199fcbf0d4a029
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-01 16:33:43 +0900

    add: リストをDoneにしてから2秒間操作がなかったら、そのリストを削除する機能

--- src/ts/Modules/RemoveList/RemoveList.ts
+++ src/ts/Modules/RemoveList/RemoveList.ts
@@ -1,3 +1,25 @@
+import { todoUl } from '../../variables/variables.js';
+import { returnTargetList } from '../DoneList/DoneList.js';
+
+const removeList = (target: HTMLElement) => {
+  const targetList = returnTargetList(target);
+  const targetButton = targetList?.querySelector('.todo__button');
+  let timeoutID = undefined;
+  timeoutID = setTimeout(() => {
+    targetList?.remove();
+  }, 2000);
+  if (!targetButton?.classList.contains('todo__button--done')) {
+    clearTimeout(timeoutID - 1);
+    clearTimeout(timeoutID);
+  }
+};
 export const RemoveList = () => {
-  console.log("RemoveList");
+  todoUl?.addEventListener('touchend', (e) => {
+    const target = e.target as HTMLElement;
+    removeList(target);
+  });
+  todoUl?.addEventListener('mouseup', (e) => {
+    const target = e.target as HTMLElement;
+    removeList(target);
+  });
 };
