f44951785f75b7c4859fd2bfdcf66261ee04a7c9
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-01 20:36:43 +0900

    add: リストを掴む機能

--- dist/Modules/GrabList/GrabList.js
+++ dist/Modules/GrabList/GrabList.js
@@ -1,34 +1,34 @@
-import { todoLists, todoUl } from '../../variables/variables.js';
-import { returnTargetList } from '../DoneList/DoneList.js';
+import { todoLists, todoUl } from "../../variables/variables.js";
+import { returnTargetList } from "../DoneList/DoneList.js";
 const grabList = (target) => {
     const targetList = returnTargetList(target);
     setTimeout(() => {
-        targetList?.classList.add('todo__li--grabbing');
+        targetList?.classList.add("todo__li--grabbing");
     }, 200);
 };
 const releaseList = (target) => {
     const targetList = returnTargetList(target);
-    targetList?.classList.remove('todo__li--grabbing');
+    targetList?.classList.remove("todo__li--grabbing");
 };
 export const GrabList = () => {
     const todoListsArray = todoLists
         ? Array.from(todoLists)
         : [];
     todoListsArray.forEach((list) => {
-        list.addEventListener('touchstart', (e) => {
+        list.addEventListener("touchstart", (e) => {
             e.preventDefault();
             const target = e.target;
             grabList(target);
         });
-        list.addEventListener('touchend', (e) => {
+        list.addEventListener("touchend", (e) => {
             const target = e.target;
             releaseList(target);
         });
-        list.addEventListener('mousedown', (e) => {
+        list.addEventListener("mousedown", (e) => {
             const target = e.target;
             grabList(target);
         });
-        list.addEventListener('mouseup', (e) => {
+        list.addEventListener("mouseup", (e) => {
             const target = e.target;
             releaseList(target);
         });
--- src/ts/Modules/GrabList/GrabList.ts
+++ src/ts/Modules/GrabList/GrabList.ts
@@ -1,35 +1,35 @@
-import { todoLists, todoUl } from '../../variables/variables.js';
-import { returnTargetList } from '../DoneList/DoneList.js';
+import { todoLists, todoUl } from "../../variables/variables.js";
+import { returnTargetList } from "../DoneList/DoneList.js";
 
 const grabList = (target: HTMLElement) => {
   const targetList = returnTargetList(target);
   setTimeout(() => {
-    targetList?.classList.add('todo__li--grabbing');
+    targetList?.classList.add("todo__li--grabbing");
   }, 200);
 };
 const releaseList = (target: HTMLElement) => {
   const targetList = returnTargetList(target);
-  targetList?.classList.remove('todo__li--grabbing');
+  targetList?.classList.remove("todo__li--grabbing");
 };
 export const GrabList = () => {
   const todoListsArray = todoLists
     ? (Array.from(todoLists) as Array<HTMLLIElement>)
     : [];
   todoListsArray.forEach((list) => {
-    list.addEventListener('touchstart', (e) => {
+    list.addEventListener("touchstart", (e) => {
       e.preventDefault();
       const target = e.target as HTMLElement;
       grabList(target);
     });
-    list.addEventListener('touchend', (e) => {
+    list.addEventListener("touchend", (e) => {
       const target = e.target as HTMLElement;
       releaseList(target);
     });
-    list.addEventListener('mousedown', (e) => {
+    list.addEventListener("mousedown", (e) => {
       const target = e.target as HTMLElement;
       grabList(target);
     });
-    list.addEventListener('mouseup', (e) => {
+    list.addEventListener("mouseup", (e) => {
       const target = e.target as HTMLElement;
       releaseList(target);
     });
