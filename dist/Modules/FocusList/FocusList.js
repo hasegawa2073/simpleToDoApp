ef71ad66df125b57665642d14bc3bd6c040c8801
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-03 21:33:19 +0900

    fix: Doneボタンを押したときに文字にフォーカスが当たってしまうのを修正

--- dist/Modules/FocusList/FocusList.js
+++ dist/Modules/FocusList/FocusList.js
@@ -1,22 +1,31 @@
-import { todoUl } from "../../variables/variables.js";
-import { returnTargetList } from "../DoneList/DoneList.js";
+import { todoLists, todoUl } from '../../variables/variables.js';
+import { returnTargetList } from '../DoneList/DoneList.js';
 export const FocusList = () => {
-    todoUl?.addEventListener("touchend", (e) => {
-        const target = e.target;
-        const targetList = returnTargetList(target);
-        const targetNode = targetList?.querySelector(".todo__text")
-            ?.firstChild;
-        const targetText = targetList?.querySelector(".todo__text");
-        const targetTextLength = targetText.textContent?.length;
-        const selection = window.getSelection();
-        const range = document.createRange();
-        range.setStart(targetNode, targetTextLength);
-        range.setEnd(targetNode, targetTextLength);
-        selection?.removeAllRanges();
-        selection?.addRange(range);
-        if (target.classList.contains("todo__content") ||
-            target.classList.contains("todo__text")) {
-            targetText.focus();
-        }
+    const todoListsArray = todoLists
+        ? Array.from(todoLists)
+        : [];
+    todoListsArray.forEach((list) => {
+        list.children[1].addEventListener('touchend', (e) => {
+            const target = e.target;
+            const targetList = returnTargetList(target);
+            const targetNode = targetList?.querySelector('.todo__text')
+                ?.firstChild;
+            const targetText = targetList?.querySelector('.todo__text');
+            const targetTextLength = targetText.textContent?.length;
+            const selection = window.getSelection();
+            const range = document.createRange();
+            range.setStart(targetNode, targetTextLength);
+            range.setEnd(targetNode, targetTextLength);
+            selection?.removeAllRanges();
+            selection?.addRange(range);
+            if (target.classList.contains('todo__content') ||
+                target.classList.contains('todo__text')) {
+                targetText.focus();
+            }
+        });
     });
 };
+const targetNode = todoUl;
+const config = { childList: true };
+const observer = new MutationObserver(FocusList);
+observer.observe(targetNode, config);
--- src/ts/Modules/FocusList/FocusList.ts
+++ src/ts/Modules/FocusList/FocusList.ts
@@ -1,25 +1,36 @@
-import { todoUl } from "../../variables/variables.js";
-import { returnTargetList } from "../DoneList/DoneList.js";
+import { todoLists, todoUl } from '../../variables/variables.js';
+import { returnTargetList } from '../DoneList/DoneList.js';
 
 export const FocusList = () => {
-  todoUl?.addEventListener("touchend", (e) => {
-    const target = e.target as HTMLElement;
-    const targetList = returnTargetList(target);
-    const targetNode = targetList?.querySelector(".todo__text")
-      ?.firstChild as Node;
-    const targetText = targetList?.querySelector(".todo__text") as HTMLElement;
-    const targetTextLength = targetText.textContent?.length as number;
-    const selection = window.getSelection();
-    const range = document.createRange();
-    range.setStart(targetNode, targetTextLength);
-    range.setEnd(targetNode, targetTextLength);
-    selection?.removeAllRanges();
-    selection?.addRange(range);
-    if (
-      target.classList.contains("todo__content") ||
-      target.classList.contains("todo__text")
-    ) {
-      targetText.focus();
-    }
+  const todoListsArray = todoLists
+    ? (Array.from(todoLists) as Array<HTMLLIElement>)
+    : [];
+  todoListsArray.forEach((list) => {
+    list.children[1].addEventListener('touchend', (e) => {
+      const target = e.target as HTMLElement;
+      const targetList = returnTargetList(target);
+      const targetNode = targetList?.querySelector('.todo__text')
+        ?.firstChild as Node;
+      const targetText = targetList?.querySelector(
+        '.todo__text'
+      ) as HTMLElement;
+      const targetTextLength = targetText.textContent?.length as number;
+      const selection = window.getSelection();
+      const range = document.createRange();
+      range.setStart(targetNode, targetTextLength);
+      range.setEnd(targetNode, targetTextLength);
+      selection?.removeAllRanges();
+      selection?.addRange(range);
+      if (
+        target.classList.contains('todo__content') ||
+        target.classList.contains('todo__text')
+      ) {
+        targetText.focus();
+      }
+    });
   });
 };
+const targetNode = todoUl as Node;
+const config = { childList: true };
+const observer = new MutationObserver(FocusList);
+observer.observe(targetNode, config);
